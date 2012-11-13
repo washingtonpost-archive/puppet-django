7492f4c53137f65375f94bca0a8bbb943420ac1d
Jason Bartz <jrbartz@gmail.com>
2012-11-12 20:55:02 -0500

    Adding better path for django apps

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class django::params {
     $init_venv_script='/usr/local/bin/virtualenv'
     $init_venv_script_path='/usr/local/bin/'
     $path=["/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin/"]
-    $location="/opt/"
+    $location="/opt/python/"
     $user="ubuntu"
     $python_c_packages=['python-dev', 'libxml2-dev', 'libxslt-dev', 'postgresql', 'postgresql-server-dev-9.1']
     $extra_c_packages=[]
